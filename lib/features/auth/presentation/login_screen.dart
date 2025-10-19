8b07b6d877b663ef1821d31b52bac85299aa7750
muhammedshahbasheer <muhammedshah7232@gmail.com>
2025-10-20 00:46:37 +0530

    refact: resolve styling issue in login screen and home screen

--- lib/features/auth/presentation/login_screen.dart
+++ lib/features/auth/presentation/login_screen.dart
@@ -20,89 +20,95 @@ class _LoginScreenState extends State<LoginScreen> {
     final authProvider = Provider.of<AuthController>(context);
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Text(
-            "Enter Your\nMobile Number",
-            style: GoogleFonts.montserrat(fontSize: 22, color: Colors.white),
-          ),
-          Text(
-            "orem ipsum dolor sit amet consectetur. Porta at id hac \n vitae. Et tortor at vehicula euismod mi viverra.",
-            style: GoogleFonts.montserrat(color: Colors.white, fontSize: 12),
-          ),
-          Row(
-            children: [
-              DropdownButton<String>(
-                value: Selectedcode,
-                items: countryCode.map((code) {
-                  return DropdownMenuItem(
-                    value: code,
-                    child: Text(
-                      code,
-                      style: GoogleFonts.montserrat(
-                        fontSize: 16,
-                        color: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 120,left: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Enter Your\nMobile Number",
+              style: GoogleFonts.montserrat(fontSize: 23, color: Colors.white),
+            ),SizedBox(height: 10,),
+            Text(
+              "orem ipsum dolor sit amet consectetur. Porta at id hac \n vitae. Et tortor at vehicula euismod mi viverra.",
+              style: GoogleFonts.montserrat(color: Colors.white, fontSize: 12),
+            ),SizedBox(height: 10),
+            Row(
+              children: [
+                DropdownButton<String>(dropdownColor: Colors.black,
+                  value: Selectedcode,
+                  items: countryCode.map((code) {
+                    return DropdownMenuItem(
+                      value: code,
+                      child: Text(
+                        code,
+                        style: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    Selectedcode = value!;
-                  });
-                },
-              ),
-              Expanded(
-                child: TextField(
-                  controller: phonecontroller,
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(hintText: "enter phone number"),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      Selectedcode = value!;
+                    });
+                  },
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: phonecontroller,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(hintText: "enter phone number"),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 80),
+            Center(
+              child: ElevatedButton(
+                onPressed: context.read<AuthController>().isLoading
+                    ? null
+                    : () async {
+                        final phone = phonecontroller.text.trim();
+                        if (phone.isEmpty || phone.length < 9) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("please enter a valid number"),
+                            ),
+                          );
+                          return;
+                        }
+                        final success = await context.read<AuthController>().login(
+                          countrycode: Selectedcode,
+                          phone: phone,
+                        );
+                        if (success && context.mounted) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomeScreen()),
+                          );
+                        }
+                      },
+                style: ElevatedButton.styleFrom(minimumSize: const Size(120, 50),backgroundColor: Colors.transparent),
+                child: Consumer<AuthController>(
+                  builder: (context, auth, _) {
+                    return auth.isLoading
+                        ? const SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                              strokeWidth: 2,
+                            ),
+                          )
+                        : const Text("Login",style: TextStyle(color: Colors.white),);
+                  },
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: context.read<AuthController>().isLoading
-                ? null
-                : () async {
-                    final phone = phonecontroller.text.trim();
-                    if (phone.isEmpty || phone.length < 9) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("please enter a valid number"),
-                        ),
-                      );
-                      return;
-                    }
-                    final success = await context.read<AuthController>().login(
-                      countrycode: Selectedcode,
-                      phone: phone,
-                    );
-                    if (success && context.mounted) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeScreen()),
-                      );
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(minimumSize: const Size(120,50,)),
-            child: Consumer<AuthController>(
-              builder: (context, auth, _) {
-                return auth.isLoading
-                    ? const SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: CircularProgressIndicator(
-                          color: Colors.white,
-                          strokeWidth: 2,
-                        ),
-                      )
-                    : const Text("Login");
-              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
